984a8d5e5bc10b463c416a72791704925d67e65a
George barber <George443001@gmail.com>
2022-03-29 08:02:59 -0400

    added some c#

---
+++ CSharp/BitsPlease/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+
+namespace BitsPlease
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* TODO LIST 
+             * create an array
+             * split that array
+             * make a method \ function 
+             *change the method to use a param
+             * change the console colors based on statuses
+             * use a physical path 
+             * 
+             
+             
+             
+             */
+// create a function \ Method 
+            static void Valadate (string computer)
+            {
+                string TestPath = @"\\"+computer+@"\c$\users";
+                if (System.IO.Directory.Exists(TestPath))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(" [On  line]  ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.Write(" [Off line]  ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(computer);
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            // make an array of computer names
+            string[] computers = { "barber-pc", "SQA1", "SQA2", "SQA3", "SQA4", "SQA-WS-A", "SQA-WS-B" , "Odin"};
+            /*string[] desktop = { "computers[0]", "computers[1]" };
+            string[] servers = { "computers[2]", "computers[3]", "computers[4]", "computers[5]", "computers[6]" };
+                       
+            Console.WriteLine("Computers in array " + desktop.Length);
+            Console.WriteLine("Servers in array " + computers.Length);*/
+            // make a foreach loop to cycle through computer names
+            foreach (string computer in computers) {
+                Valadate(computer);
+                
+            }
+
+           
+        }
+    }
+}
---
+++ CSharp/textplay/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+public class Example
+{
+    public static void Main()
+    {
+        string path = @"e:\somefile.txt";
+
+        bool fileExist = File.Exists(path);
+        if (fileExist)
+        {
+            Console.WriteLine("File exists.");
+        }
+        else
+        {
+            Console.WriteLine("File does not exist.");
+            Console.WriteLine("do you wnat me to make it (y \\ n)");
+            string answer = Console.ReadLine();
+            if (answer == "y")
+            {
+                using (FileStream fs = File.Create(path)) ;
+            }
+
+        }
+    }
+}
