bb1bc896c976911838b89651447ea6b865de7e58
george b <george443001@outlook.com>
2022-07-25 18:44:47 -0400

    7/25/22

---
+++ CSharp/CsharpClassWork.unitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CsharpClassWork.unitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CsharpClassWork.unitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("77dc17d8-2265-4895-b9f1-dca5bafb6509")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- CSharp/CsharpClassWork/Program.cs
+++ CSharp/CsharpClassWork/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Moq;
+
+
 
 namespace CsharpClassWork
 {
@@ -24,8 +29,8 @@ namespace CsharpClassWork
             */
             int         myNum            = 5;
             double  myDoubleNum = 5.99D;
-            float      totalCost           = 15.22f;
-            char      grade                 = 'D';
+            float totalCost           = 15.22f;
+            char  grade                 = 'D';
             bool      myBool             = true;
             string    myText              = "Hello";
 
@@ -36,10 +41,52 @@ namespace CsharpClassWork
             Console.WriteLine(myBool);
             Console.WriteLine(myText);
         }
+        static void WorkingWithLists() {
+            /*working with lists*/
+
+            // define a new list with four numbers 
+            var ListNumbers = new List<int>() { 1, 2, 3, 4 };
+
+            // add the number 5 to the list
+            ListNumbers.Add(5);
+
+            // add numbers from an array to the current list of numbers
+            ListNumbers.AddRange(new int[3] { 6, 7, 8 });
+
+            // write to the console all of the numbers in this list 
+            foreach (var num in ListNumbers) { Console.ForegroundColor = ConsoleColor.Yellow; Console.WriteLine(num); }
+
+            // write to the console the contents index 1 in the the list 
+            Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine(ListNumbers[1]);
+
+            // remove index [0] of the list and watch the rest of the list adjust. 
+            Console.ForegroundColor = ConsoleColor.DarkCyan; Console.WriteLine("Was this " + ListNumbers[1]);
+            ListNumbers.Remove(1);
+            Console.WriteLine("now its  this " + ListNumbers[1]);
+
+        }
+        static void List1Numbers() {
+            var ListNumbers1 = new List<int>() { 1, 2, 3, 4 };
+            while (0 == 0)
+            {
+
+                int newNumber = ListNumbers1.Count + 1;
+                ListNumbers1.Add(newNumber);
+                //Console.ReadLine();
+                foreach (var n in ListNumbers1)
+                {
+                    Console.WriteLine(n);
+
+                }
+
+            }
+        }
+
+        
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
